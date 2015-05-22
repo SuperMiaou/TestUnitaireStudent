40aa7d45ff8ce506337a519ff5a5039a60a76545
SuperMiaou <chante.matthieu@gmail.com>
2015-05-22 14:34:34 +0200

    Initial Commit

---
+++ TestUnitaireStudent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUnitaireStudent
+//
+//  Created by Matthieu Chante on 22/05/2015.
+//  Copyright (c) 2015 Matthieu Chante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestUnitaireStudent/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestUnitaireStudent
+//
+//  Created by Matthieu Chante on 22/05/2015.
+//  Copyright (c) 2015 Matthieu Chante. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
